17b017d3fe62935d2afff7b7b3221601ac910cf5
CHANDUMCA <chandumca234@gmail.com>
2025-12-06 23:49:05 +0530

    updated files

--- src/com/sunrizeit/interview/Pgm1_ReverseString.java
+++ src/com/sunrizeit/interview/Pgm1_ReverseString.java
@@ -1,82 +1,67 @@
 package com.sunrizeit.interview;
 
 import java.util.Collections;
-import java.util.List;
 import java.util.Scanner;
-import java.util.stream.Collector;
+
 import java.util.stream.Collectors;
 
 public class Pgm1_ReverseString {
-    public static void main(String[] args) {
-        System.out.print("Enter a String : ");
-        Scanner sc = new Scanner(System.in);
-        String input = sc.next();
-        System.out.println("Choose option 1 or 2 or 3 or 4" + " \n Method 1 using predefined" + "\n Method 2.Using Programmatically");
-        int option = sc.nextInt();
-        if (option == 1) {
-            method1(input);
-        } else if (option == 2) {
-            method2(input);
-        } else if ((option == 3)) {
-            method3(input);
-        } else if (option == 4) {
-            method4(option,input);
-        }
-        else if (option == 5) {
-            method5(option,input);
-        }
+	public static void main(String[] args) {
+		System.out.print("Enter a String : ");
+		Scanner sc = new Scanner(System.in);
+		String input = sc.next();
+		System.out.println("Choose option 1 or 2 or 3 or 4" + ""
+				+ " \n Method 1 using predefined 		(StringBuilder)"
+				+ "\n Method 2.Using Programmatically 	(using for loop)"
+				+ "\n Method 3.Using toCharArray to copy elements"
+				+ "\n Method 4.Using streams");
+		int option = sc.nextInt();
+		if (option == 1) {
+			method1(input);
+		} else if (option == 2) {
+			method2(input);
+		} else if (option == 3) {
+			method3(option, input);
+		} else if (option == 4) {
+			method4(option, input);
+		}
+		sc.close();
 
-    }
+	}
 
-    private static void method1(String input) {
-        StringBuilder output = new StringBuilder(input).reverse();
-        System.out.println(output);
-    }
+	private static void method1(String input) {
+		StringBuilder output = new StringBuilder(input).reverse();
+		System.out.println(output);
+	}
 
-    public static void method2(String input) {
-        int length = input.length();
-        String rev = "";
-        for (int le = length - 1; le >= 0; le--) {
-            rev += input.charAt(le);
-        }
-        System.out.println(rev);
-    }
+	public static void method2(String input) {
+		int length = input.length();
+		String rev = "";
+		for (int le = length - 1; le >= 0; le--) {
+			rev += input.charAt(le);
+		}
+		System.out.println(rev);
+	}
 
-    public static void method3(String input) {
-        String rev = "";
-        for (int i = 0; i < input.length(); i++) {
-            rev = input.charAt(i) + rev;
-        }
-        /*
-       We start with an empty string rev.
-        - On each loop, we take one character from the original string using charAt().
-        - Instead of adding it to the end, we place it in front of the existing rev.
-        - This way, the characters are built in reverse order.
-        For example, from "Hello" we get "olleH".
-       */
-    }
+	public static void method3(int option, String input) {
+		System.out.println("selected method " + option);// Using toCharArray to copy elements
+		char[] arr = input.toCharArray();
+		for (int i = arr.length - 1; i >= 0; i--)
+			System.out.print(arr[i]);
+	}
 
-    public static void method4(int option ,String input) {
-        System.out.println("selected method "+option);
-        // Using toCharArray to copy elements
-        char[] arr = input.toCharArray();
-        for (int i = arr.length - 1; i >= 0; i--)
-            System.out.print(arr[i]);
-    }
-    
-    //Using streams
-    public static void method5(int option, String input) {
-    	List<Character> lis=input.chars().mapToObj(c->(char) c).collect(Collectors.toList());
-    	System.out.println(lis);
-    	Collections.reverse(lis);
-    	System.out.println(lis.stream()
-    	        .map(String::valueOf)
-    	        .collect(Collectors.joining()));
-    	
-    }
+	// Using streams
+	public static void method4(int option, String input) {
+		System.out.println("using method streams "+option);
+		String reversedStream = input.chars()		// Create an IntStream of characters
+				.mapToObj(c -> (char) c) 			// Convert int to Character
+				.collect(Collectors.toList()) 		// Collect characters into a list
+				.stream() 							// Create a stream again
+	            .sorted(Collections.reverseOrder()) // Reverse the order of the stream
+				.map(String::valueOf) 				// Convert each character to a string
+				.collect(Collectors.joining());		// Join them back together into a single string
+		System.out.println(reversedStream);			// Output: 
+
+	}
+	
 }
-
-
-
-
-
--- src/com/sunrizeit/interview/Pgm3_StringRemoveSpace.java
+++ src/com/sunrizeit/interview/Pgm3_StringRemoveSpace.java
@@ -1,9 +1,34 @@
 package com.sunrizeit.interview;
 
+import java.util.Scanner;
+
 public class Pgm3_StringRemoveSpace {
 
 	public static void main(String[] args) {
-		System.out.println("Test Git Upload");
+		System.out.println("Program to reverse a String");
+		Scanner sc = new Scanner(System.in);
+		System.out.println(
+				"This ouput we can achieve different ways :\n 1.Using program \n 2.Using stream \n 3.Using stream \n Choose the option from the above : ");
+		int select = sc.nextInt();
+		sc.nextLine();
+		switch (select) {
+		case 1: {
+			System.out.println("Enter your input : ");
+			String input = sc.nextLine();
+			String output = input.replace(" ", "");
+			System.out.println("output : " + output);
+			sc.close();
+			break;
+		}
+		case 2: {
+			System.out.println("its in development");
+			break;
+		}
+		default:
+			System.out.println("Invalid option");
+			break;
+
+		}
 
 	}
 
